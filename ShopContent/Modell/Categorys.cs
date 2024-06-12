45287874ec8c3402ed8994d20894b6b73bd40b67
Hat <kitarkomar@gmail.com>
2024-06-13 02:07:18 +0500

    Разработанные модели данных. Kurlishuk

---
+++ ShopContent/Modell/Categorys.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Runtime.CompilerServices;
+using System.Xml.Linq;
+
+namespace ShopContent.Modell
+{
+    public class Categorys : INotifyPropertyChanged
+    {
+        private int id;
+        public int Id
+        {
+            // аксессор чтения
+            get { return id; }
+            set
+            {
+                id = value; // записываем значение
+                OnPropertyChanged("Id"); // сообщаем об изменении
+            }
+        }
+        /// <summary> Поле наименования
+        private string name;
+
+        public string Name
+        {
+            // аксессор чтения
+            get { return name; }
+            // аксессор записи
+            set
+            {
+                name = value; // записываем значение
+                OnPropertyChanged("Name"); // сообщаем об изменении
+            }
+        }
+        /// <summary> Событие изменения свойства
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ ShopContent/Modell/Items.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Runtime.CompilerServices;
+using System.Diagnostics;
+using System.Xml.Linq;
+
+namespace ShopContent.Modell
+{
+    public class Items : INotifyPropertyChanged
+    {
+        private int id;
+        public int Id
+        {
+            // аксессор для чтения
+            get { return id; }
+            // аксессор для записи
+            set
+            {
+                id = value; // записываем значение
+                OnPropertyChanged("Id"); // сообщаем о том что свойство изменилось
+            }
+        }
+        /// <summary> Поле наименования
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value; // записываем значение
+                OnPropertyChanged("Name");
+            }
+        }
+
+
+        private double price;
+
+        public double Price
+        {
+            // аксессор для чтения
+            get { return price; }
+            // аксессор для записи
+            set
+            {
+                price = value; // записываем значение
+                OnPropertyChanged("Price"); // сообщаем о том что свойство изменилось
+            }
+        }
+        private string description;
+
+        public string Description
+        {
+            // аксессор для чтения
+            get { return description; }
+            // аксессор для записи
+            set
+            {
+                description = value; // записываем значение
+                OnPropertyChanged("Description"); // сообщаем о том что свойство изменилось
+            }
+        }
+        /// <summary> Категория к которой относится предмет
+        private Categorys category;
+        public Categorys Category
+        {
+            // аксессор для чтения
+            get { return category; }
+            // аксессор для записи
+            set
+            {
+                category = value; // записываем значение
+                OnPropertyChanged("Category"); // сообщаем о том что свойство изменилось
+            }
+        }
+        /// <summary> Событие изменения свойства
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
