53e924f22eb670e3862514bc20a1edd50cc7bd2e
Hat <kitarkomar@gmail.com>
2024-06-13 01:30:29 +0500

    Разработанные классы общего пользования. Kurlishuk

---
+++ ShopContent/Classes/Connection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace ShopContent.Classes
+{
+    public class Connection
+    {
+        private static readonly string config = "server=student.permaviat.ru;" +
+            "Trusted_Connection=No;" +
+            "DataBase=ShopContent;" +
+            "User=***;" +
+            "PWD=***";
+        public static SqlConnection OpenConnection()
+        {
+            // Создаём поключение к базе данных
+            SqlConnection connection = new SqlConnection(config);
+            // Открываем соединение
+            connection.Open();
+            // Возвращаем соединение
+            return connection;
+        }
+
+        public static SqlDataReader Query(string SQL, out SqlConnection connection)
+        {
+            // Создаём подключение к базе данных
+            connection = OpenConnection();
+            // Возвращаем данные
+            return new SqlCommand(SQL, connection).ExecuteReader();
+        }
+
+        public static void CloseConnection(SqlConnection connection)
+        {
+            // Закрываем подключение
+            connection.Close();
+            SqlConnection.ClearPool(connection);
+        }
+    }
+}
---
+++ ShopContent/Classes/RelayCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Windows.Input;
+
+namespace ShopContent.Classes
+{
+    internal class RelayCommand : ICommand
+    {
+        private Action<object> execute;
+        /// <summary> Возможность выполнения метода
+        private Func<object, bool> canExecute;
+        /// <summary> Конструктор для регистрации выполняемого метода
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null || this.canExecute(parameter);
+        }
+        public void Execute(object parameter) =>
+            this.execute(parameter);
+    }
+}
