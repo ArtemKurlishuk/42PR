47c336ebb80d1db972f722b69b1bd0f51edbb3c1
Hat <kitarkomar@gmail.com>
2024-06-13 04:04:07 +0500

    Разработанные контексты данных. Kurlishuk

---
+++ ShopContent/Context/CategorysContext.cs
@@ -0,0 +1,29 @@
+﻿using ShopContent.Modell;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ShopContent.Classes;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+
+namespace ShopContent.Context
+{
+    public class CategorysContext : Categorys
+    {
+        public static ObservableCollection<CategorysContext> AllCategoys()
+        {
+            ObservableCollection<CategorysContext> allCategoys = new ObservableCollection<CategorysContext>();
+            SqlConnection connection;
+            SqlDataReader dataCategotys = Connection.Query("SELECT * FROM [dbo].[Categorys]", out connection);
+            while (dataCategotys.Read())
+            {
+                allCategoys.Add(new CategorysContext()
+                {
+                    Id = dataCategotys.GetInt32(0),
+                    Name = dataCategotys.GetString(1)
+                });
+            }
+            Connection.CloseConnection(connection); return allCategoys;
+        }
+    }
+}
---
+++ ShopContent/ViewModell/VMItems.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+
+namespace ShopContent.ViewModell
+{
+    public class VMItems : INotifyPropertyChanged
+    {
+        public ObjectDisposedException<Context.ItemsContext> Items {  get; set; }
+        public Classes.RelayCommand NewItems
+        {
+            get
+            {
+                return new Classes.RelayCommand(obj =>
+                {
+                    Context.ItemsContext newModell = new Context.ItemsContext(true);
+                    Items.Add(newModell);
+                    MainWindow.init.frame.Navigate(new View.Add(newModell));
+                });
+            }
+        }
+        public VMItems() =>
+            Items = Context.ItemsContext.AllItems();
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnProrertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null) 
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+
+}
